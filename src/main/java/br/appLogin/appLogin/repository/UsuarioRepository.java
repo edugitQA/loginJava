93f5478c8844c5b264f624a88fb5d9ab0ac14601
edugitqa <eduardo.telecomrbs@gmail.com>
2025-03-06 00:03:26 -0300

    back_front sistema login

--- src/main/java/br/appLogin/appLogin/repository/UsuarioRepository.java
+++ src/main/java/br/appLogin/appLogin/repository/UsuarioRepository.java
@@ -4,5 +4,5 @@ import br.appLogin.appLogin.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository  extends CrudRepository<Usuario, String> {
-    
+
 }
